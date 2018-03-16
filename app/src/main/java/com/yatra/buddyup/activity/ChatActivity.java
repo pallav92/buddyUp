b3ac549fc9bad42843439156b36e582781548962
YATRAONLINE\v-pallav.srivastava <v-pallav.srivastava@yatra.com>
2018-03-16 17:18:38 +0530

    Objects added

--- app/src/main/java/com/yatra/buddyup/activity/ChatActivity.java
+++ app/src/main/java/com/yatra/buddyup/activity/ChatActivity.java
@@ -22,6 +22,7 @@ import com.yatra.buddyup.model.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -41,9 +42,7 @@ public class ChatActivity extends AppCompatActivity {
         initializeUI();
 
         database = FirebaseDatabase.getInstance();
-        mDatabaseRef = database.getReference("message");
-
-        mDatabaseRef.setValue("Hello, World!");
+        mDatabaseRef = database.getReference("main");
 
         ArrayList<String> interest = new ArrayList<>();
         interest.add("Travel");
@@ -97,14 +96,16 @@ public class ChatActivity extends AppCompatActivity {
 
     private void enterNewMessage(Message message, ChatRoom chatRoom){
         String key = mDatabaseRef.child("chatrooms").child(chatRoom.getChatRoomId()).push().getKey();
-        mDatabaseRef.child("chatrooms").child(chatRoom.getChatRoomId()).child("messages/"+key).setValue(message);
+        Map<String, Object> postValues = chatRoom.toMap();
+        mDatabaseRef.child("/chatrooms").child(chatRoom.getChatRoomId()).child("messages/"+key).setValue(message);
     }
 
 
     private void writeNewUser( String userId,String name, String email, List<String> interests) {
         User user = new User(userId,name,email, "", interests);
         String key = mDatabaseRef.child("users").push().getKey();
-        mDatabaseRef.child("/users").child(key).setValue(user);
+        user.setUserId(key);
+        mDatabaseRef.child("/users").child(user.getName()).setValue(user);
     }
 
     private List<Message> getDummyMessages() {
