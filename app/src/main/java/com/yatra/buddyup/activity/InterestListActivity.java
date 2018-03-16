bf68fa3639d5c3518352d6b342dedc12e5148aa6
Kumar Gaurav <kumar.gaurav@yatra.com>
2018-03-16 20:41:52 +0530

    - Fixes

--- app/src/main/java/com/yatra/buddyup/activity/InterestListActivity.java
+++ app/src/main/java/com/yatra/buddyup/activity/InterestListActivity.java
@@ -1,39 +1,33 @@
 package com.yatra.buddyup.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.yatra.buddyup.R;
-import com.yatra.buddyup.model.User;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class InterestListActivity extends AppCompatActivity {
 
 
     private FirebaseDatabase database;
     private DatabaseReference mDatabaseRef;
-    ArrayList<String>interesList= new ArrayList<>();
+    ArrayList<String> interesList = new ArrayList<>();
+    private View lastSelectedView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interest_list);
-        setTitle("Choose Your Interest");
-        setTitleColor(R.color.colorPrimary);
         database = FirebaseDatabase.getInstance();
         mDatabaseRef = database.getReference("main/users");
-       
         initViews();
     }
 
@@ -42,36 +36,36 @@ public class InterestListActivity extends AppCompatActivity {
     }
 
 
-
-    public  void onClickOFInterest(View view){
-        if(view.getTag() == null){
-            view.setTag(true);
-        }else {
-            view.setTag(!((boolean) view.getTag()));
-        }
-        if(((boolean) view.getTag())){
-            view.setBackground(ContextCompat.getDrawable(this,android.R.color.holo_blue_light));
+    public void onClickOFInterest(View view) {
+        if (lastSelectedView == null) {
+            lastSelectedView = view;
+            view.setBackground(ContextCompat.getDrawable(this, android.R.color.holo_blue_light));
             view.setElevation(300);
-
-        }else{
-            view.setBackground(ContextCompat.getDrawable(this,android.R.color.white));
-            view.setElevation(0);
-
-
+        } else {
+            lastSelectedView.setBackground(ContextCompat.getDrawable(this, android.R.color.white));
+            lastSelectedView.setElevation(0);
+            lastSelectedView.setTag(false);
+            view.setBackground(ContextCompat.getDrawable(this, android.R.color.holo_blue_light));
+            view.setElevation(300);
+            lastSelectedView = view;
         }
-
-
     }
 
     public void onClickOfSubmit(View view) {
-        submitUserInterest(new ArrayList<String>());
+        LinearLayout selectedView = (LinearLayout) lastSelectedView;
+        String selectedInterest = ((String) lastSelectedView.getTag());
+        submitUserInterest(selectedInterest);
         finish();
     }
 
-    private void submitUserInterest( List<String> interests) {
-        interests.add("Movies");
-        interests.add("Pets");
-        interests.add("Health");
-        mDatabaseRef.child("Pallav").child("interests").setValue(interests);
+    private void submitUserInterest(String interest) {
+        if (lastSelectedView == null) {
+            Toast.makeText(this, "Please choose one interest", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        Intent chatIntent = new Intent(this, ChatActivity.class);
+        chatIntent.putExtra("interest", interest);
+        startActivity(chatIntent);
     }
 }
